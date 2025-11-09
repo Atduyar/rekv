d6a32b93d271fb0a073817bcce581c423e9719ad
Ahmet Tarik Duyar(Atduyar) <atduyar@gmail.com>
2025-11-10 00:27:33 +0100

    feat: Make thread safe db

--- src/db.rs
+++ src/db.rs
@@ -1,26 +1,31 @@
 #![allow(dead_code, unused)]
 use crate::kv_value::KVValue;
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Mutex};
 
 type Key = String;
 type Value = KVValue;
 
-pub struct DB(HashMap<Key, Value>);
+pub struct DB {
+    db: Mutex<HashMap<Key, Value>>
+}
 
 impl DB {
     pub fn new() -> DB {
-        DB(HashMap::new())
+        DB { db: Mutex::new(HashMap::new()) }
     }
 
-    pub fn get(&self, key: &Key) -> Option<&Value> {
-        self.0.get(key)
+    pub fn get(&self, key: &Key) -> Option<Value> {
+        let mut db = self.db.lock().unwrap();
+        db.get(key).cloned()
     }
 
     pub fn insert(&mut self, key: Key, value: Value) -> Option<Value> {
-        self.0.insert(key, value)
+        let mut db = self.db.lock().unwrap();
+        db.insert(key, value)
     }
 
     pub fn remove(&mut self, key: &Key) -> Option<Value> {
-        self.0.remove(key)
+        let mut db = self.db.lock().unwrap();
+        db.remove(key)
     }
 }
